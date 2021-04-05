e6a5f569321ece299cd5edfc4bae8123d940fcb6
OskarRosen <oskar_rosenqvist@hotmail.com>
2021-04-05 14:50:49 +0200

    Increased accuracy

--- main.py
+++ main.py
@@ -21,31 +21,68 @@ def erode(image):
     return cv2.erode(image, kernel, iterations=1)
 
 
-def image_preprocessing(image):
-    image = get_grayscale(image)
-    image = thresholding(image)
-    image = erode(image)
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
 
-    return image
+
+def dilate(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.dilate(image, kernel, iterations=1)
+
+
+# opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+
+# skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+
+def image_preprocessing(image, tries):
+    if tries == 1:
+        image = get_grayscale(image)
+    elif tries == 2:
+        image = thresholding(image)
+    elif tries == 3:
+        image = erode(image)
+    elif tries == 4:
+        image = opening(image)
+    elif tries == 5:
+        image = deskew(image)
+    tries += 1
+    return image, tries
 
 
 def get_tesseract_data(image):
     ocr_data = image_to_data(image, config=custom_config, output_type=Output.DICT)
-
+    print(ocr_data)
     return ocr_data
 
 
-def search_tesseract_data(data, text_pattern, num_pattern):
-    letters = ''
-    numbers = ''
+def search_tesseract_data(data, text_pattern, num_pattern, letters, numbers):
+
     index = 0
     lx, ly, nx, ny, nw, nh = 0, 0, 0, 0, 0, 0
     for i in data['text']:
-        if re.search(text_pattern, i):
+        if re.search(text_pattern, i) and data['conf'][index] > 50:
             letters = i
             lx, ly, lw, lh = data['left'][index], data['top'][index], data['width'][
                 index], data['height'][index]
-        elif re.search(num_pattern, i):
+        elif re.search(num_pattern, i) and data['conf'][index] > 50:
             numbers = i
             nx, ny, nw, nh = data['left'][index], data['top'][index], data['width'][
                 index], data['height'][index]
@@ -60,8 +97,10 @@ def search_tesseract_data(data, text_pattern, num_pattern):
 
     rectangle_pos1 = (lx, ly)
     rectangle_pos2 = (nx + nw, ny + nh)
-    letters = re.sub(r'[^A-Z]', '', letters)
-    numbers = re.sub(r'[^0-9]', '', numbers)
+    # letters = re.sub(r'[^A-Z]', '', letters)
+    letters = re.sub(r'\W', '', letters)
+    # numbers = re.sub(r'[^0-9]', '', numbers)
+    numbers = re.sub(r'\W', '', numbers)
 
     return letters, numbers, rectangle_pos1, rectangle_pos2
 
@@ -82,14 +121,19 @@ def draw_license_plate_pos(image, letters, numbers, rectangle_pos1, rectangle_po
 
 
 def main():
-    img = cv2.imread('bil2.jpeg')
-    img2 = image_preprocessing(img)
-
-    tesseract_data = get_tesseract_data(img2)
-
-    license_plate_letters, license_plate_numbers, coord1, coord2 = search_tesseract_data(
-        tesseract_data, data_pattern_letters, data_pattern_numbers)
-
+    img = cv2.imread('bil2.jpg')
+    img2 = cv2.imread('bil2.jpg')
+    attempt = 1
+    done = False
+    letters = 'NOTHING'
+    numbers = ''
+    while not done:
+        img2, attempt = image_preprocessing(img2, attempt)
+        tesseract_data = get_tesseract_data(img2)
+        license_plate_letters, license_plate_numbers, coord1, coord2 = search_tesseract_data(
+            tesseract_data, data_pattern_letters, data_pattern_numbers, letters, numbers)
+        if (re.match('^[A-Z][A-Z][A-Z]$', license_plate_letters) and re.match('^[0-9][0-9][0-9]$', license_plate_numbers)) or attempt > 6:
+            done = True
     draw_license_plate_pos(img, license_plate_letters, license_plate_numbers, coord1, coord2)
 
 
